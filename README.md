428baed3a3a58dd1c7cb1fc4ee1cab0508be2e34
pangluo <59036386@qq.com>
2019-09-08 11:56:02 +0800

    删除目录文件

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # apiFrameDomo
-今天刚把代码从SVN搬到github
-使用及介绍没得时间写
 
-慢慢来
+**工程导入**
+ - sqlserver.zip文件解压出来放置在 C:\Users\Test\.m2\repository\com\microsoft【你的maven本地仓库】
+ - dingding.zip C:\Users\Test\.m2\repository\com\alibaba 【你的maven本地仓库】
+ - 这样pom.xml就不会提示找不到maven坐标
--- lib/mvn.bat
+++ lib/mvn.bat
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=taobao-sdk-java-auto_1479188381469-20190719.jar -DgroupId=com.alibaba.dingding DartifactId=dingdingtalk -Dversion=1.0 -Dpackaging=jar
 
--Dfile=sqljdbc4.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jarmvn install:install-file
+mvn install:install-file -Dfile=sqljdbc4.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar
